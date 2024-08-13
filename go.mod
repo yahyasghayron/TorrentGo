18959160d75833a069db134603d4acc28a98ccfb
yahyasghayron <yahya.sghayron123@gmail.com>
2024-08-13 23:20:29 +0100

    add go.mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yahyasghayron/TorrentGo
+
+go 1.22.6
--- main.go
+++ main.go
@@ -3,10 +3,6 @@ package main
 import "fmt"
 
 func main(){
-  fmt.Println("Hello World")
-  foo()
+  fmt.Println("hello world")
 }
 
- func foo(){
-  fmt.Println("foo")
-}
